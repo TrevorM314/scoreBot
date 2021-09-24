a84ae4cd401b3a5a23e3fc44476d38ae0b782dbf
trevor <trevor.g.mitchell@gmail.com>
2021-09-23 21:29:53 -0400

    Test route

--- app.js
+++ app.js
@@ -4,53 +4,53 @@ const fs = require("fs");
 require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = parseInt(process.env.PORT) || 3000;
 
-const botId = process.env.BOT_ID;
+//const botId = process.env.BOT_ID;
 
 app.use(bodyParser.json());
 
-app.get('/', (res) => {
+app.get('/', (req, res) => {
   res.send("COMING SOON");
-  res.send(fs.readFileSync("./score.json", "utf-8"));
+  //npmres.send(fs.readFileSync("./score.json", "utf-8"));
 });
 
-app.post('/', (req, res) => {
-  let text = req.body.text;
-  let data = req.body;
-  let commands = {
-    hello: /^\/say hello$/,
-    add: /^\/add$/
-  }
-  if (!text || !data.sender_id) {
-    throw new Error("Text and user_id needed")
-  }
-  let scores = fs.readFileSync("./score.json", "utf-8");
-  scores = JSON.parse(scores);
+// app.post('/', (req, res) => {
+//   let text = req.body.text;
+//   let data = req.body;
+//   let commands = {
+//     hello: /^\/say hello$/,
+//     add: /^\/add$/
+//   }
+//   if (!text || !data.sender_id) {
+//     throw new Error("Text and user_id needed")
+//   }
+//   let scores = fs.readFileSync("./score.json", "utf-8");
+//   scores = JSON.parse(scores);
 
-  if (text.match(commands.hello)) {
-    res.send(req.body);
-  }
+//   if (text.match(commands.hello)) {
+//     res.send(req.body);
+//   }
 
-  else if (text.match(commands.add)) {
-    let user_id = data.sender_id;
-    let name = data.name;
-    if (scores[user_id] !== undefined) {
-      scores[user_id]["score"] ++;
-    } else {
-      scores[user_id] = {
-        name,
-        score: 1
-      }
-    }
-    fs.writeFileSync("./score.json", JSON.stringify(scores, null, 2));
-    answer(name, scores[user_id]["score"])
-  }
+//   else if (text.match(commands.add)) {
+//     let user_id = data.sender_id;
+//     let name = data.name;
+//     if (scores[user_id] !== undefined) {
+//       scores[user_id]["score"] ++;
+//     } else {
+//       scores[user_id] = {
+//         name,
+//         score: 1
+//       }
+//     }
+//     fs.writeFileSync("./score.json", JSON.stringify(scores, null, 2));
+//     answer(name, scores[user_id]["score"])
+//   }
 
-  else {
-    res.send("Could not understand");
-  }
-})
+//   else {
+//     res.send("Could not understand");
+//   }
+// })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
