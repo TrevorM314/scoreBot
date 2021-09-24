a643b384799b700d7997e088df06862aae9958a8
trevor <trevor.g.mitchell@gmail.com>
2021-09-23 21:34:27 -0400

    Still debugging

--- app.js
+++ app.js
@@ -15,42 +15,43 @@ app.get('/', (req, res) => {
   //npmres.send(fs.readFileSync("./score.json", "utf-8"));
 });
 
-// app.post('/', (req, res) => {
-//   let text = req.body.text;
-//   let data = req.body;
-//   let commands = {
-//     hello: /^\/say hello$/,
-//     add: /^\/add$/
-//   }
-//   if (!text || !data.sender_id) {
-//     throw new Error("Text and user_id needed")
-//   }
-//   let scores = fs.readFileSync("./score.json", "utf-8");
-//   scores = JSON.parse(scores);
+app.post('/', (req, res) => {
+  let text = req.body.text;
+  let data = req.body;
+  let commands = {
+    hello: /^\/say hello$/,
+    add: /^\/add$/
+  }
+  
+  if (!text || !data.sender_id) {
+    throw new Error("Text and user_id needed")
+  }
+  let scores = fs.readFileSync("./score.json", "utf-8");
+  scores = JSON.parse(scores);
 
-//   if (text.match(commands.hello)) {
-//     res.send(req.body);
-//   }
+  if (text.match(commands.hello)) {
+    res.send(req.body);
+  }
 
-//   else if (text.match(commands.add)) {
-//     let user_id = data.sender_id;
-//     let name = data.name;
-//     if (scores[user_id] !== undefined) {
-//       scores[user_id]["score"] ++;
-//     } else {
-//       scores[user_id] = {
-//         name,
-//         score: 1
-//       }
-//     }
-//     fs.writeFileSync("./score.json", JSON.stringify(scores, null, 2));
-//     answer(name, scores[user_id]["score"])
-//   }
+  else if (text.match(commands.add)) {
+    let user_id = data.sender_id;
+    let name = data.name;
+    if (scores[user_id] !== undefined) {
+      scores[user_id]["score"] ++;
+    } else {
+      scores[user_id] = {
+        name,
+        score: 1
+      }
+    }
+    fs.writeFileSync("./score.json", JSON.stringify(scores, null, 2));
+    answer(name, scores[user_id]["score"])
+  }
 
-//   else {
-//     res.send("Could not understand");
-//   }
-// })
+  else {
+    res.send("Could not understand");
+  }
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
